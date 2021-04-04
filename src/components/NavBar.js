04a35d1ed52249be75a14a4f5324b09267f10686
Grzegorz Cieslik <grzegorz.cieslik@hotmail.com>
2021-04-04 08:13:27 +0200

    navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,25 @@
 import React from "react";
-
+import { Navlink } from "react-router-dom";
+ 
 export default function NavBar() {
-    return <h1>Navbar</h1>
+    return (
+    <header>
+        <div>
+            <nav>
+                <Navlink to = "/" exact>
+                    Home
+                </Navlink>
+                <Navlink to = "/post">
+                    Blog
+                </Navlink>
+                <Navlink to = "/project">
+                    Project
+                </Navlink>
+                <Navlink to = "/about">
+                    About Me
+                </Navlink>
+            </nav>
+        </div>
+    </header>
+    )
 }
