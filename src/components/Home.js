0629a63048ff30524fb6e634a78aeb107e6e95b3
Grzegorz Cieslik <grzegorz.cieslik@hotmail.com>
2021-04-05 09:52:01 +0200

    some minor edit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,17 @@
 import React from "react";
 
 export default function Home() {
-    return <h1>Home!</h1>
+    return (
+        <main>
+            <section>
+                <div>
+                    <h2>Never Stop</h2>
+                    <h3>Exploring The World</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo</p>
+                </div>
+            </section>
+        </main>
+    )
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,30 +8,32 @@ export default function NavBar() {
         <header className="bg-gray-600">
             <div className="container flex justify-between">
                 <nav className="flex flex-auto items-center px-20">
-                    <NavLink to="/" exact
+                    <NavLink id="RouterNavLinkHome" to="/" exact
                         activeClassName="text-gray-300"
                         className="inflex-flex items-center py-3 px-3 mr-4 text-white transform hover:scale-110 motion-reduce:transform-none text-2xl tracking-widest">
                         Home
                     </NavLink>
-                    <NavLink to="/post"
+                    <NavLink id="RouterNavLinkPost" to="/post"
                         activeClassName="text-gray-300"
                         className="inflex-flex items-center py-3 px-3 mr-4 text-white transform hover:scale-110 motion-reduce:transform-none text-2xl tracking-widest">
                         Blog
                     </NavLink>
-                    <NavLink to="/project"
+                    <NavLink id="RouterNavLinkProject" to="/project"
                         activeClassName="text-gray-300"
                         className="inflex-flex items-center py-3 px-3 mr-4 text-white transform hover:scale-110 motion-reduce:transform-none text-2xl tracking-widest">
                         Project
                     </NavLink>
-                    <NavLink to="/about"
+                    <NavLink id="RouterNavLinkAbout" to="/about"
                         activeClassName="text-gray-300"
                         className="inflex-flex items-center py-3 px-3 mr-4 text-white transform hover:scale-110 motion-reduce:transform-none text-2xl tracking-widest">
                         About Me
                     </NavLink>
                 </nav>
-                <div className = "py-2">
-                <SocialIcon url="https://github.com/GrzegorzCieslik" style={{ height: 60, width: 60 }} className="mr-3" fgColor="white" bgColor="transparent" />
-                <SocialIcon url="mailto:grzegorz.cieslik@hotmail.com" style={{ height: 60, width: 60 }} className="mr-3" fgColor="white" bgColor="transparent"/>
+                <div className="py-2">
+                    <SocialIcon url="https://github.com/GrzegorzCieslik" style={{ height: 60, width: 60 }} className="mr-3 transform hover:-translate-y-1" fgColor="white" bgColor="transparent" />
+                    <NavLink id="RouterNavLinkContact" to="/contact">
+                        <SocialIcon url="mailto:grzegorz.cieslik@hotmail.com" style={{ height: 60, width: 60 }} className="mr-3 transform hover:-translate-y-1" fgColor="white" bgColor="transparent" />
+                    </NavLink>
                 </div>
             </div>
         </header>
